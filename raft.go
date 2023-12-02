a1e5abe0e80d757c0a128159cf239d450ca89be9
Yiwei Chen <y77chen@gmail.com>
2023-12-02 15:54:28 -0600

    Added message data & size profile

--- raft.go
+++ raft.go
@@ -1242,6 +1242,12 @@ func stepLeader(r *raft, m pb.Message) error {
 		})
 		return nil
 	case pb.MsgProp:
+		// Profile the data and data size
+		for i := range m.Entries {
+			e := &m.Entries[i]
+			r.logger.Info("MSG_PROFILE: length: %d", len(e.Data))
+			r.logger.Infof("MSG_PROFILE: data: " + string(e.Data))
+		}
 		if len(m.Entries) == 0 {
 			r.logger.Panicf("%x stepped empty MsgProp", r.id)
 		}
